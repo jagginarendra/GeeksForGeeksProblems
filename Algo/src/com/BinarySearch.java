37698f474c6535d250d36b8e2b83a0d9d97e5b99
jagginarendra <jagginarendra@gmail.com>
2015-08-24 03:05:51 +0530

    Binary Search

---
+++ Algo/src/com/BinarySearch.java
@@ -0,0 +1,59 @@
+package com;
+
+
+public class BinarySearch {
+
+	private int size;
+	private int array[];
+	private int i = 0;
+	
+	public BinarySearch(int n){
+		size = n;
+		array = new int[size];
+	}
+	
+	public void insert(int item){
+		
+		if(i < size){
+			
+			array[i++] = item;
+		}
+		else
+			System.out.println("Array Full");
+	}
+	
+	public void binarySearch(int item){
+		
+		int first = 0,last = size -1 ,mid = 0;
+		int counter  = 0;
+		while(first <= last){
+			counter++;
+			mid  = (first+last)/2;
+			if(array[mid] == item)
+			{
+				System.out.println("Item found @ "+ ++mid);
+				System.out.println("Counter "+ counter);
+				break;
+			}
+			else{
+				
+				if (array[mid] > item)
+				{
+					last = mid - 1;
+				}
+				else{
+					first = mid + 1;
+				}
+				
+			}
+			
+		}
+		if(first > last)
+		{
+			System.out.println("Item is not present");
+			System.out.println("Counter "+ counter);
+		}
+	}
+	
+	
+}
